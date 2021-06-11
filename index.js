405c0566140b814bd9f7fde971e59107fb58fdfe
Dhineshamt <a.dhinesh1@gmail.com>
2021-06-11 22:41:48 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'testdb',
+    password: '1234',
+    port: 5432,
+  })
+
+  client.connect()
+  client.query('SELECT * FROM COMPANY', (err, res) => {
+    console.log(err, res)
+    client.end()
+  })
+
+  // client.connect()
+  // client.query('SELECT * FROM COMPANY', (err, res) => {
+  //   console.log(err, res)
+  //   client.end()
+  // })
+
+// client.query('DELETE FROM COMPANY WHERE id=1', (err, res) => {
+//   console.log(err, res);
+//   client.end();
+// })
