7976d0b46644e780d9e8460d474c252938a5fcb8
Dhineshamt <a.dhinesh1@gmail.com>
2021-06-18 12:27:12 +0530

    adding postgre code

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require("pg");
+
+// const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb';
+
+const connectionString = "postgresql://postgres:1234@localhost:5432/testdb";
 // url: https://node-postgres.com/features/connecting
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testdb',
-    password: '1234',
-    port: 5432,
-  })
+const client = new Client(
+  { connectionString }
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'testdb',
+  // password: '1234',
+  // port: 5432,
+);
 
-  client.connect()
-  client.query('SELECT * FROM COMPANY', (err, res) => {
-    console.log(err, res)
-    client.end()
-  })
+// client.connect()
+// client.query('SELECT * FROM COMPANY', (err, res) => {
+//   console.log(res);
+//   // if(err) {
+//   //   console.error("Error triggered");
+//   // }
+//   client.end()
+// })
 
-  // client.connect()
-  // client.query('SELECT * FROM COMPANY', (err, res) => {
-  //   console.log(err, res)
-  //   client.end()
-  // })
+// const runQuery = {
+//   name: "Insert",
+//   text: "INSERT INTO COMPANY VALUES($1, $2, $3, $4, $5)",
+//   values: [6, "Dhinesh", 29, "NC", 5000],
+// };
+// client.connect();
+// client.query(runQuery, (err, res) => {
+//   console.log("Result:", res, "Error: ", err );
+//   client.end();
+// });
+
+const delQuery = {
+  name: "Delete",
+  text: "DELETE FROM COMPANY WHERE id=6"
+}
+
+client.connect();
+
+client.query(delQuery, (err, res) => {
+  console.log(err, res);
+  client.end();
+});
+
+// client.connect()
+// client.query('SELECT * FROM COMPANY', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
 
 // client.query('DELETE FROM COMPANY WHERE id=1', (err, res) => {
 //   console.log(err, res);
